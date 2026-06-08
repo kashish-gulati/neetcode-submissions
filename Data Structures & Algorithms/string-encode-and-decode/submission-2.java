71cf55100364fa395cb34d68e5f4caa80a39d71a
Kashish Gulati <kashlati@gmail.com>
2026-06-08 21:15:46 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder encoded = new StringBuilder();
+        for(String s: strs) {
+            encoded.append(s.length()).append('#').append(s);
+        }
+        return encoded.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> dec = new ArrayList<>();
+        int count=0;
+        boolean n=true;
+        StringBuilder s = new StringBuilder();
+        for(char ch: str.toCharArray()) {
+            if(n) {
+                if(ch=='#') {
+                    if(count!=0) {
+                        n=false;
+                    } else {
+                        dec.add("");
+                    }
+                } else {
+                    count=count*10+(ch-'0');
+                }
+            } else {
+                s.append(ch);
+                count--;
+                if(count==0) {
+                    n = true;
+                    dec.add(s.toString());
+                    s.setLength(0);
+                }
+            }
+        }
+        return dec;
+    }
+}
