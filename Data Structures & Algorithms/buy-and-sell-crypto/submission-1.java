b0425a6496bb38f0002d16e6e17dac49332b5ac7
Kashish Gulati <kashlati@gmail.com>
2026-06-30 19:31:48 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        //if inc continue
+        //if dec
+            //calc profit and comp with max
+            //calc new min
+        int min=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.length;i++) {
+            if(prices[i-1]>prices[i]) {
+                profit = Math.max(profit,prices[i-1]-min);
+                min = Math.min(min,prices[i]);
+            }
+        }
+        profit = Math.max(profit,prices[prices.length-1]-min);
+        return profit;
+    }
+}
